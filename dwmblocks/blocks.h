a40225624d9a0033c9fd82ec4ca93e60f65893f9
siggmar <subberman0822@gmail.com>
2025-11-10 13:08:47 +0100

    l

---
+++ dwmblocks/blocks.h
@@ -0,0 +1,12 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+
+	{"", "date '+%I:%M'",					5,		0},
+    {"", "/home/jay/projects/dwmblocks/scripts/battery", 30, 0}
+
+};
+
+//sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
