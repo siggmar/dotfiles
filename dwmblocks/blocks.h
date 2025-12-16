a6c92dd154aed2297f06b32d14b6c61d743a7ec8
siggmar <subberman0822@gmail.com>
2025-12-16 11:35:14 +0100

    added network to bar

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -3,8 +3,9 @@
 static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
 
-    {"", "date '+%I:%M'", 5, 0},
-    {"", "~/dotfiles/dwmblocks/scripts/battery", 30, 0}};
+    {"", "~/dotfiles/dwmblocks/scripts/battery", 30, 0},
+    {"", "~/dotfiles/dwmblocks/scripts/network", 30, 0},
+    {"", "date '+%I:%M'", 5, 0}};
 
 // sets delimiter between status commands. NULL character ('\0') means no
 // delimiter.
