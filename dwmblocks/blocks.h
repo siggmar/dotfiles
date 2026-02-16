57f81f52b74a1dea13975dd546c8629ea5fe2fa8
siggmar <subberman0822@gmail.com>
2026-02-16 21:03:11 +0100

    new status scripts

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -3,11 +3,15 @@
 static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
 
+    // {"", "~/dotfiles/dwmblocks/scripts/ram", 5, 0},
+    // {"", "~/dotfiles/dwmblocks/scripts/disk", 60, 0},
+    // {"", "~/dotfiles/dwmblocks/scripts/temperature", 10, 0},
     {"", "~/dotfiles/dwmblocks/scripts/battery", 30, 0},
-    {"", "~/dotfiles/dwmblocks/scripts/network", 30, 0},
-    {"", "date '+%I:%M'", 5, 0}};
+    {"", "~/dotfiles/dwmblocks/scripts/network", 5, 2},
+    {"", "~/dotfiles/dwmblocks/scripts/date", 1, 3}
+};
 
 // sets delimiter between status commands. NULL character ('\0') means no
 // delimiter.
 static char delim[] = " | ";
-static unsigned int delimLen = 5;
+static unsigned int delimLen = 7;
